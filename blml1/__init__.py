c5b51107226974723e29e22e5ac2f25d8d0bf3dd
kshramt <kshramt@example.com>
2020-08-01 11:31:42 +0900

    Support the `shuffle=` and `repeat=` arguments

--- blml1/__init__.py
+++ blml1/__init__.py
@@ -23,16 +23,24 @@ import optuna.integration.lightgbm
 from ._common import logger
 
 
-__version__ = "0.10.0"
+__version__ = "0.11.0"
 _T1 = TypeVar("_T1")
 
 
 class DataIteratorV1(Generic[_T1]):
-    def __init__(self, xs: Sequence[_T1], random_state: int):
+    def __init__(
+        self,
+        xs: Sequence[_T1],
+        random_state: int,
+        shuffle: bool = True,
+        repeat: bool = True,
+    ):
         if not xs:
             raise ValueError("`xs` should have at least one element.")
         self._xs = xs
         self._rng = random.Random(random_state)
+        self._shuffle = shuffle
+        self._repeat = repeat
         self._n = len(self._xs)
         self._i = -1
         self._inds = list(range(self._n))
@@ -41,19 +49,29 @@ class DataIteratorV1(Generic[_T1]):
         return self
 
     def __next__(self) -> _T1:
-        self._i = (self._i + 1) % self._n
-        if self._i == 0:
+        self._i += 1
+        if self._repeat:
+            self._i %= self._n
+        elif self._n <= self._i:
+            raise StopIteration
+        if self._shuffle and (self._i == 0):
             self._rng.shuffle(self._inds)
         return self._xs[self._inds[self._i]]
 
 
 class DataIterableV1(Generic[_T1]):
-    def __init__(self, xs: Sequence[_T1], random_state: int):
+    def __init__(
+        self, xs: Sequence[_T1], random_state: int, shuffle: bool = True, repeat=True
+    ):
         self._xs = xs
         self._random_state = random_state
+        self._shuffle = shuffle
+        self._repeat = repeat
 
     def __iter__(self) -> DataIteratorV1[_T1]:
-        return DataIteratorV1(self._xs, self._random_state)
+        return DataIteratorV1(
+            self._xs, self._random_state, shuffle=self._shuffle, repeat=self._repeat
+        )
 
 
 class LabelEncoderV1(Generic[_T1]):
